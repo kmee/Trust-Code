7f53a8c484fa81e2dfe93a54cdf6b577d2cb4c57
Mackilem <mack.vdl@gmail.com>
2014-10-30 16:41:31 -0200

    sale report customizado

---
+++ sale_report/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+
+{
+	"name" : "Sale Reports",
+	"version" : "1.2",
+	"description" : "Report sample for report_aeroo module",
+	"author" : "Alistek Ltd",
+    'website': 'http://www.alistek.com',
+	"depends" : ["base", "report_aeroo"],
+	"category" : "Isotelha",
+	"init_xml" : [],
+	"demo_xml" : [],
+	"data" : ["report/report_sale.xml"],
+	"installable": True,
+    "license" : "GPL-3 or any later version",
+}
+
