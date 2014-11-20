6f2c239438ab2dd6ad01434e49a1e33583f4caa4
Mackilem <mack.vdl@gmail.com>
2014-11-20 16:18:27 -0200

    corrigido o nome da classe

--- cotation_back_draft/sale_order.py
+++ cotation_back_draft/sale_order.py
@@ -2,7 +2,7 @@
 
 from openerp.osv import orm, fields
 
-class sale_contact(orm.Model):
+class sale_back_draft(orm.Model):
     _inherit = 'sale.order'
     
     def button_back_draft(self, cr, uid, ids, context=None):
